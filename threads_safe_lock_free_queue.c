6de4cb353b9f1a92c3f7fd772787953dfd101ada
HAN Guodong <onthewinghgd@gmail.com>
2013-10-28 14:00:40 -0400

    Update threads_safe_lock_free_queue.c
    Use CAS to implement lock-free based queue operations

--- threads_safe_lock_free_queue.c
+++ threads_safe_lock_free_queue.c
@@ -1,1 +1,52 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+  int valie;
+  struct node *next;
+  };
+typedef struct node* nodeptr;
+
+nodeptr head=NULL;
+nodeptr tail=NULL;
+
+//atomic operation CAS
+bool CAS(nodeptr addr, nodeptr old, nodeptr new){
+ if(addr==old){
+   add=new;
+   return true;
+ }
+ else
+  return false;
+}
+
+
+void enqueue(nodeptr p){
+ if(CAS(tail,NULL,p)){
+   head=p;
+   return;
+ }
+ nodeptr q;
+ while(1){
+   q=tail;
+   if(CAS(q->next,NULL,p))
+     break;
+   else 
+    CAS(tail,q,q->next);
+ }
+ CAS(tail,q,p);
+}
+
+
+nodeptr dequeue(){
+  nodeptr p;
+  while(1){
+    if(head==NULL){
+      tail=NULL;
+      return tail;
+    }
+    p=head;
+    if(CAS(head,p,p->next))
+     return p;
+  }
+}
